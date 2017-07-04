2ea168f49a790119b1a1cbeb510f58acb3ad4c7c
coldrunKacper <kacper@coldrun.pl>
2017-07-04 22:37:29 +0200

    HW-339: Updating export_renderers.js with download tsv/csv features.

--- js/pivottable/export_renderers.js
+++ js/pivottable/export_renderers.js
@@ -1,3 +1,11 @@
+function getcurrenttimestamp() {
+  var now = new Date();
+  var date = [now.getFullYear(), now.getMonth() + 1, now.getDate()];
+  var time = [now.getHours(), now.getMinutes()];
+
+  return date.join('') + '_' + time.join('');
+}
+
 (function() {
   var callWithJQuery;
 
@@ -67,14 +75,73 @@
           r = result[n];
           text += r.join("\t") + "\n";
         }
-        return $("<textarea>").text(text).css({
-          width: ($(window).width() / 2) + "px",
-          height: ($(window).height() / 2) + "px"
+
+        return $('<a id="download" href="data:text/tsv,' + encodeURIComponent(text) + '"> Download as a TSV File </a>').click(function() {
+          $('#download').attr('download', 'pivot_report_' + getcurrenttimestamp() + '.tsv');
+        });
+     },
+
+    "CSV Export": function(pivotData, opts) {
+        var agg, colAttrs, colKey, colKeys, defaults, i, j, k, l, len, len1, len2, len3, len4, len5, m, n, r, result, row, rowAttr, rowAttrs, rowKey, rowKeys, text;
+        defaults = {
+          localeStrings: {}
+        };
+        opts = $.extend(defaults, opts);
+        rowKeys = pivotData.getRowKeys();
+        if (rowKeys.length === 0) {
+          rowKeys.push([]);
+        }
+        colKeys = pivotData.getColKeys();
+        if (colKeys.length === 0) {
+          colKeys.push([]);
+        }
+        rowAttrs = pivotData.rowAttrs;
+        colAttrs = pivotData.colAttrs;
+        result = [];
+        row = [];
+        for (i = 0, len = rowAttrs.length; i < len; i++) {
+          rowAttr = rowAttrs[i];
+          row.push(rowAttr);
+        }
+        if (colKeys.length === 1 && colKeys[0].length === 0) {
+          row.push(pivotData.aggregatorName);
+        } else {
+          for (j = 0, len1 = colKeys.length; j < len1; j++) {
+            colKey = colKeys[j];
+            row.push(colKey.join("-"));
+          }
+        }
+        result.push(row);
+        for (k = 0, len2 = rowKeys.length; k < len2; k++) {
+          rowKey = rowKeys[k];
+          row = [];
+          for (l = 0, len3 = rowKey.length; l < len3; l++) {
+            r = rowKey[l];
+            row.push(r);
+          }
+          for (m = 0, len4 = colKeys.length; m < len4; m++) {
+            colKey = colKeys[m];
+            agg = pivotData.getAggregator(rowKey, colKey);
+            if (agg.value() != null) {
+              row.push(agg.value());
+            } else {
+              row.push("");
+            }
+          }
+          result.push(row);
+        }
+        text = "";
+        for (n = 0, len5 = result.length; n < len5; n++) {
+          r = result[n];
+          text += r.join(",") + "\n";
+        }
+
+        return $('<a id="download" href="data:text/csv,' + encodeURIComponent(text) + '"> Download as a CSV File </a>').click(function() {
+          $('#download').attr('download', 'pivot_report_' + getcurrenttimestamp() + '.csv');
         });
       }
     };
   });
-
 }).call(this);
 
 //# sourceMappingURL=export_renderers.js.map
