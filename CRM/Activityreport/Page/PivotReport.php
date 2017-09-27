73af9e0d855f8c5d885620c792d854b53788d129
coldrunKacper <kacper@coldrun.pl>
2017-09-27 21:18:02 +0200

    GS-65: Refactoring routing and Entity backend handling.

---
+++ CRM/Activityreport/Page/PivotReport.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'CRM/Core/Page.php';
+
+class CRM_Activityreport_Page_PivotReport extends CRM_Core_Page {
+  function run() {
+    $args = func_get_args();
+
+    $entity = !empty($args[1]['entity']) ? $args[1]['entity'] : NULL;
+    $entityInstance = new CRM_Activityreport_Entity($entity);
+    $supportedEntities = CRM_Activityreport_Entity::getSupportedEntities();
+
+    CRM_Utils_System::setTitle(ts('Pivot Report'));
+
+    $this->assign('reportTitle', ts($entity . ' Report'));
+    $this->assign('options_array', array_combine($supportedEntities, $supportedEntities));
+    $this->assign('CRMDataType', $entity);
+
+    parent::run();
+  }
+}
