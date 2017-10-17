ba531c754475195bf7d14585f5e6750d573b2cc3
Kacper Warda <kacper@compucorp.co.uk>
2017-10-17 17:57:12 +0200

    GS-73: Updating classes to make DataProspect extend DataCase class.

--- CRM/PivotReport/DataProspect.php
+++ CRM/PivotReport/DataProspect.php
@@ -3,7 +3,7 @@
 /**
  * Provides a functionality to prepare Prospect data for Pivot Table.
  */
-class CRM_PivotReport_DataProspect extends CRM_PivotReport_AbstractData {
+class CRM_PivotReport_DataProspect extends CRM_PivotReport_DataCase {
 
   /**
    * CRM_PivotReport_DataProspect constructor.
@@ -28,20 +28,6 @@ class CRM_PivotReport_DataProspect extends CRM_PivotReport_AbstractData {
     );
 
     return $params;
-  }
-
-  protected function getCaseFields() {
-    $result = array();
-    $fields = array_keys($this->getFields());
-
-    foreach ($fields as $field) {
-      $fieldParts = explode('.', $field);
-      if ($fieldParts[0] === 'case') {
-        $result[] = $fieldParts[1];
-      }
-    }
-
-    return $result;
   }
 
   /**
@@ -92,149 +78,6 @@ class CRM_PivotReport_DataProspect extends CRM_PivotReport_AbstractData {
     }
 
     return $result;
-  }
-
-  /**
-   * Returns Prospect related Case fields and values.
-   *
-   * @param array $data
-   *
-   * @return array
-   */
-  protected function getCaseValues($data) {
-    $result = array();
-    $fields = $this->getFields();
-    $include = array('id', 'status_id', 'start_date', 'end_date');
-
-    foreach ($data as $key => $value) {
-      $resultKey = 'case.' . $key;
-      if (empty($fields[$resultKey])) {
-        continue;
-      }
-
-      if (in_array($key, $include) || CRM_Utils_String::startsWith($key, 'custom_')) {
-        $result[$resultKey] = $value;
-      }
-    }
-
-    return $result;
-  }
-
-  /**
-   * Returns Prospect related fields and values basing on specified entity name.
-   *
-   * @param array $data
-   * @param string $entityName
-   *
-   * @return array
-   */
-  protected function getRowValues($data, $entityName) {
-    $result = array();
-    $fields = $this->getFields();
-
-    foreach ($data as $key => $value) {
-      $fieldsKey = $entityName . '.' . $key;
-
-      if (empty($fields[$fieldsKey])) {
-        continue;
-      }
-
-      $resultKey = $fieldsKey;
-      if (!is_array($fields[$fieldsKey])) {
-        $resultKey = $fields[$fieldsKey];
-      }
-
-      $result[$resultKey] = $value;
-    }
-
-    return $result;
-  }
-
-  /**
-   * Returns an array containing manager label as key and manager display
-   * name as value.
-   *
-   * @param array $contacts
-   *
-   * @return array
-   */
-  protected function getManager($contacts) {
-    foreach ($contacts as $contact) {
-      if (!empty($contact['manager']) && (int) $contact['manager'] === 1) {
-        return array(
-          ts('Case Manager Display Name') => $contact['display_name'],
-        );
-      }
-    }
-
-    return array(
-      ts('Case Manager Display Name') => '',
-    );
-  }
-
-  /**
-   * Returns an array containing formatted rows of specified array.
-   *
-   * @param int $key
-   * @param array $row
-   *
-   * @return array
-   */
-  protected function formatRow($key, $row) {
-    $fields = $this->getFields();
-    $result = array();
-
-    foreach ($row as $key => $value) {
-      $label = $key;
-      if (!empty($fields[$key]['title'])) {
-        $label = $fields[$key]['title'];
-      }
-      $label = ts($label);
-
-      $formattedValue = $this->formatValue($key, $value);
-      $result[$label] = $formattedValue;
-
-      if (is_array($formattedValue)) {
-        $this->multiValues[$key][] = $label;
-      }
-    }
-
-    ksort($result);
-
-    return $result;
-  }
-
-  /**
-   * @inheritdoc
-   */
-  protected function setCustomValue($key, $value) {
-    $result = $value;
-
-    switch ($key) {
-      case 'campaign_id':
-        if (!empty($value)) {
-          $campaign = civicrm_api3('Campaign', 'getsingle', array(
-            'sequential' => 1,
-            'return' => 'title',
-            'id' => $value,
-          ));
-          if ($campaign['is_error']) {
-            $result = '';
-          } else {
-            $result = $campaign['title'];
-          }
-        }
-      break;
-    }
-
-    $this->customizedValues[$key][$value] = $result;
-  }
-
-  /**
-   * @inheritdoc
-   */
-  protected function getEntityIndex(array $row) {
-    return NULL;
   }
 
   /**
@@ -335,33 +178,5 @@ class CRM_PivotReport_DataProspect extends CRM_PivotReport_AbstractData {
     }
 
     return $this->fields;
-  }
-
-  /**
-   * Returns available Option Values of specified $field array.
-   * If there is no available Option Values for the field, then return null.
-   *
-   * @param array $field
-   *   Field key
-   *
-   * @return array
-   */
-  private function getOptionValues($field) {
-    if (empty($field['pseudoconstant']['optionGroupName'])) {
-      return null;
-    }
-
-    $result = civicrm_api3('Case', 'getoptions', array(
-      'field' => $field['name'],
-    ));
-
-    return $result['values'];
-  }
-
-  /**
-   * @inheritdoc
-   */
-  protected function getCount(array $params) {
-    return civicrm_api3('ProspectConverted', 'getcount', array());
   }
 }
