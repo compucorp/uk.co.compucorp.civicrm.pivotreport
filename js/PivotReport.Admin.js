8d507aa73fe558c2911f4287f844ab5708777a73
Kacper Warda <kacper@compucorp.co.uk>
2017-11-08 21:54:21 +0100

    GS-91: Creating PivotReport.Admin JS class to handle Admin page events.

--- js/PivotReport.Admin.js
+++ js/PivotReport.Admin.js
@@ -1,236 +1,139 @@
 CRM.PivotReport = CRM.PivotReport || {};
 
-CRM.PivotReport.Config = (function($) {
+CRM.PivotReport.Admin = (function($) {
 
   /**
-   * Initializes Pivot Config.
-   *
-   * @param {object} PivotTable
+   * Initializes Pivot Admin.
    */
-  function Config(PivotTable) {
-    this.PivotTable = PivotTable;
-    this.pivotConfig = {};
-    this.container = $('#pivot-report-config');
+  function Admin() {
+    this.container = $('#pivot-report-admin');
+    this.totalCount = 0;
+    this.loadedCount = {};
+    this.Preloader = new CRM.PivotReport.Preloader();
+    this.Preloader.hide();
 
     this.initUI();
   };
 
   /**
-   * Sets Pivot Config object value.
-   *
-   * @param {object} pivotConfig
+   * Initializes of cache building.
    */
-  Config.prototype.setPivotConfig = function(pivotConfig) {
-    this.pivotConfig = pivotConfig;
-  };
-
-  /**
-   * Gets Pivot Config object value.
-   *
-   * @returns {object}
-   */
-  Config.prototype.getPivotConfig = function() {
-    return this.pivotConfig;
-  };
-
-  /**
-   * Handles both Save and SaveNew actions server requests.
-   *
-   * @param {int} configId
-   * @param {string} configLabel
-   */
-  Config.prototype.configSaveProcess = function(configId, configLabel) {
+  Admin.prototype.buildCache = function() {
     var that = this;
 
-    CRM.api3('PivotReportConfig', 'create', {
-      'id': configId,
-      'entity': that.PivotTable.getEntityName(),
-      'label': configLabel,
-      'json_config': JSON.stringify(that.getPivotConfig())
+    CRM.api3('PivotReport', 'gettotalcount', {
+      'sequential': 1
     }).done(function(result) {
-      if (result.is_error) {
-        CRM.alert(result.message, 'Error saving Report configuration', 'error');
-        return;
-      }
-
-      if (!configId) {
-        $('.report-config-select', that.container).append('<option value="' + result.id + '">' + result.values[result.id].label + '</option>');
-        var emptyOption = $('.report-config-select option[value=""]', this.container).text();
-        $('.report-config-select option[value=""]', that.container).remove();
-
-        // Sort options by their labels alphabetically.
-        $('.report-config-select', that.container).append($(".report-config-select option").remove().sort(function(a, b) {
-          var at = $(a).text().toLocaleLowerCase(), bt = $(b).text().toLocaleLowerCase();
-          return (at > bt) ? 1 : ((at < bt) ? -1 : 0);
-        }));
-
-        $('.report-config-select', that.container).prepend('<option value="">' + emptyOption + '</option>');
-        $('.report-config-select', that.container).val(result.id);
-
-        $('.report-config-save-btn', that.container).show();
-      }
-
-      CRM.alert('Report configuration has been saved', 'Success', 'success');
-    });
-  };
-
-  /**
-   * Returns an ID of currently active Report configuration.
-   *
-   * @returns {int}
-   */
-  Config.prototype.getReportConfigurationId = function() {
-    return $('.report-config-select', this.container).val();
-  };
-
-  /**
-   * Applies given Pivot Table configuration.
-   *
-   * @param {object} config
-   */
-  Config.prototype.applyConfig = function(config) {
-    var that = this;
-
-    config['onRefresh'] = function (config) {
-      return that.PivotTable.pivotTableOnRefresh(config);
-    }
-
-    this.PivotTable.applyConfig(config);
-  };
-
-  /**
-   * Gets Report configuration by currently selected configId
-   * and apply it to the Pivot Table instance.
-   */
-  Config.prototype.configGet = function() {
-    var that = this;
-    var configId = this.getReportConfigurationId();
-    if (!configId) {
-      $('.report-config-save-btn', this.container).hide();
-      return false;
-    }
-
-    $('.report-config-save-btn', this.container).show();
-
-    CRM.api3('PivotReportConfig', 'getsingle', {
-      'id': configId
-    }).done(function(result) {
-      if (result.is_error) {
-        CRM.alert(result.error_message, 'Error loading Pivot Report configuration', 'error');
-        return;
-      }
-
-      that.applyConfig(JSON.parse(result.json_config));
-      CRM.alert('Pivot Report configuration applied.', '', 'info');
-    });
-  };
-
-  /**
-   * Saves Report configuration with currently selected configId.
-   *
-   * @param {string} message
-   */
-  Config.prototype.configSave = function(message) {
-    var that = this;
-    var configId = this.getReportConfigurationId();
-    if (!configId) {
-      CRM.alert('Please choose configuration to update.', 'No configuration selected', 'error');
-      return false;
-    }
-
-    if (typeof message === 'undefined') {
-      message = 'Are you sure you want to save this configuration changes?';
-    }
-
-    CRM.confirm({
-      'message': message
-    }).on('crmConfirm:yes', function() {
-      that.configSaveProcess(configId);
-    })
-  };
-
-  /**
-   * Saves new Report configuration basing on currently set configuration.
-   */
-  Config.prototype.configSaveNew = function() {
-    var that = this;
-
-    swal({
-      title: 'New Report configuration',
-      text: 'Configuration name:',
-      type: 'input',
-      showCancelButton: true,
-      closeOnConfirm: true,
-      inputPlaceholder: 'New configuration label'
-    }, function (inputValue) {
-      if (inputValue === false) {
-        return false;
-      }
-
-      if (inputValue === '') {
-        swal.showInputError('Configuration name cannot be empty.');
-        return false;
-      }
-
-      that.configSaveProcess(0, inputValue);
+      that.totalCount = result.values;
+      that.cacheAllEntities();
     });
   }
 
   /**
-   * Deletes currently active configuration.
+   * Handles supported entities caching.
    */
-  Config.prototype.configDelete = function() {
+  Admin.prototype.cacheAllEntities = function() {
     var that = this;
 
-    var configId = this.getReportConfigurationId();
-    if (!configId) {
-      CRM.alert('Please choose configuration to delete.', 'No configuration selected', 'error');
-      return false;
-    }
+    CRM.api3('PivotReport', 'getsupportedentitiescount', {
+      'sequential': 1
+    }).done(function(result) {
+      that.Preloader.reset();
+      that.Preloader.show();
 
-    var configId = this.getReportConfigurationId();
+      for (var i in result.values[0]) {
+        that.loadedCount[i] = 0;
+        that.cacheEntity(i, 0, 0, null, 0, result.values[0][i]);
+      }
+    });
+  }
 
-    CRM.confirm({
-      'message': 'Are you sure you want to delete this configuration?'
-    }).on('crmConfirm:yes', function() {
-      CRM.api3('PivotReportConfig', 'delete', {
-        'id': configId
-      }).done(function(result) {
-        if (result.is_error) {
-          CRM.alert(result.message, 'Error deleting Report configuration', 'error');
-          return;
+  /**
+   * Builds cache for specified entity.
+   *
+   * @param {string} entity
+   * @param {int} offset
+   * @param {int} multiValuesOffset
+   * @param {string} index
+   * @param {int} page
+   * @param {int} entityCount
+   */
+  Admin.prototype.cacheEntity = function(entity, offset, multiValuesOffset, index, page, entityCount) {
+    var that = this;
+
+    CRM.api3('PivotReport', 'rebuildcachepartial', {
+        entity: entity,
+        offset: offset,
+        multiValuesOffset: multiValuesOffset,
+        index: index,
+        page: page
+    }).done(function(result) {
+      that.loadedCount[entity] = offset;
+
+      var progressValue = parseInt((that.getTotalLoadedCount() / that.totalCount) * 100, 10);
+      that.Preloader.setValue(progressValue);
+
+      if (parseInt(result.values.count, 10) === 0) {
+        that.loadedCount[entity] = entityCount;
+
+        if (that.getTotalLoadedCount() === that.totalCount) {
+          that.Preloader.hide();
+          that.updateBuildDateTime();
+
+          CRM.alert('All Pivot Reports have been refreshed.', null, 'success');
         }
 
-        $('.report-config-select option[value=' + configId + ']', this.container).remove();
-        CRM.alert('Report configuration has been deleted', 'Success', 'info');
+        return;
+      }
+
+      that.cacheEntity(entity, result.values.offset, result.values.multiValuesOffset, result.values.index, result.values.page, entityCount);
+    });
+  }
+
+  /**
+   * Returns total count of already cached items.
+   *
+   * @returns {int}
+   */
+  Admin.prototype.getTotalLoadedCount = function() {
+    var result = 0;
+
+    for (var i in this.loadedCount) {
+      result += this.loadedCount[i];
+    }
+
+    return result;
+  }
+
+  /**
+   * Updates cache build datetime.
+   */
+  Admin.prototype.updateBuildDateTime = function() {
+    var that = this;
+
+    CRM.api3('PivotReport', 'updatebuilddatetime').done(function() {
+      CRM.api3('PivotReport', 'getbuilddatetime', {
+         'sequential': 1,
+         'format': 1
+      }).done(function(result) {
+        $('div.build-date-time > span', that.container).text(result.values);
       });
     });
-  };
+  }
 
   /**
    * Handles UI events.
    */
-  Config.prototype.initUI = function() {
+  Admin.prototype.initUI = function() {
     var that = this;
 
-    $('form', this.container).on('submit', function(e) {
-      e.preventDefault();
-      return that.configSaveNew();
-    });
-
-    $('.report-config-select', this.container).bind('change', function(e) {
-      that.configGet();
-    });
-    $('.report-config-save-btn', this.container).bind('click', function(e) {
-      that.configSave();
-    });
-    $('.report-config-save-new-btn', this.container).bind('click', function(e) {
-      that.configSaveNew();
-    });
-    $('.report-config-delete-btn', this.container).bind('click', function(e) {
-      that.configDelete();
+    $('input[type="button"].build-cache-button').click(function(e) {
+      CRM.confirm({message: 'This operation may take some time to build the cache. Do you really want to build the cache for all supported entities?' })
+      .on('crmConfirm:yes', function() {
+        that.buildCache();
+      });
     });
   };
 
-  return Config;
+  return Admin;
 })(CRM.$);
