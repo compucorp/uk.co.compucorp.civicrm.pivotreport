9bc7f0e7824c132b9269e024c97c079251ed6392
Muhammad Shahrukh <>
2024-07-26 16:33:08 +0500

    COMCL-635: Upgrade civix

--- pivotreport.php
+++ pivotreport.php
@@ -12,17 +12,6 @@ function pivotreport_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function pivotreport_civicrm_xmlMenu(&$files) {
-  _pivotreport_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -32,103 +21,12 @@ function pivotreport_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function pivotreport_civicrm_uninstall() {
-  _pivotreport_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function pivotreport_civicrm_enable() {
   _pivotreport_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function pivotreport_civicrm_postInstall() {
-  _pivotreport_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function pivotreport_civicrm_disable() {
-  _pivotreport_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function pivotreport_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _pivotreport_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function pivotreport_civicrm_managed(&$entities) {
-  _pivotreport_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function pivotreport_civicrm_caseTypes(&$caseTypes) {
-  _pivotreport_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function pivotreport_civicrm_angularModules(&$angularModules) {
-_pivotreport_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function pivotreport_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _pivotreport_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -182,14 +80,4 @@ function pivotreport_civicrm_permission(&$permissions) {
     'access CiviCRM pivot table reports' => $prefix . ts('access CiviCRM pivot table reports'),
     'Admin Pivot Report' => $prefix . ts('Admin Pivot Report'),
   );
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * @param array $entityTypes
- *   List of entity types
- */
-function pivotreport_civicrm_entityTypes(&$entityTypes) {
-  _pivotreport_civix_civicrm_entityTypes($entityTypes);
 }
