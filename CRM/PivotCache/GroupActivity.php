4be0a83990b6bd0257901c5dccb7aaed9a445928
shahrukh <shahrukh@compuco.io>
2026-04-22 22:38:35 +0500

    COMCL-1440: Fix handling of large datasets in pivot reports

--- CRM/PivotCache/GroupActivity.php
+++ CRM/PivotCache/GroupActivity.php
@@ -8,35 +8,4 @@ class CRM_PivotCache_GroupActivity extends CRM_PivotCache_AbstractGroup {
   public function __construct($name = NULL, $source = NULL) {
     parent::__construct('Activity', $source);
   }
-
-  /**
-   * @inheritdoc
-   */
-  protected function customizeQuery(CRM_PivotReport_DAO_PivotReportCache $queryObject, $page, array $params) {
-    if (!empty($params['keyvalue_from'])) {
-      $whereStartDate = CRM_Core_DAO::createSQLFilter(
-        // Remove the unique suffix added to the path.
-        'LEFT(path, LENGTH(path) - 14)',
-        array(
-          '>=' => $this->getPath(substr($params['keyvalue_from'], 0, 10), $page, FALSE),
-        ),
-        'String'
-      );
-
-      $queryObject->whereAdd($whereStartDate);
-    }
-
-    if (!empty($params['keyvalue_to'])) {
-      $whereEndDate = CRM_Core_DAO::createSQLFilter(
-      // Remove the unique suffix added to the path.
-        'LEFT(path, LENGTH(path) - 14)',
-        array(
-          '<=' => $this->getPath(substr($params['keyvalue_to'], 0, 10), 999999, FALSE),
-        ),
-        'String'
-      );
-
-      $queryObject->whereAdd($whereEndDate);
-    }
-  }
 }
